c3f16b8510b0f9f96a042b8499e849092f53d605
Bahizi <bahatibahizi209@gmail.com>
2025-11-02 11:09:38 +0200

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+import ConverterForm from "./ConverterForm";
+
+
+function App() {
+return(
+  <div className="currency-converter">
+  <h2 className="converter-title">Currency Converter</h2>
+  <ConverterForm />
+  </div>
+  
+);
+}
+
+export default App
